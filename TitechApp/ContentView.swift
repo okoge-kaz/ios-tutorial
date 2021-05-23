d8eaeec423b80df32d3cae5fe343b58baf677a8b
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-23 13:14:30 +0900

    initial commit

---
+++ TitechApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechApp
+//
+//  Created by fujii kazuki on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TitechApp/TitechAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitechAppApp.swift
+//  TitechApp
+//
+//  Created by fujii kazuki on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TitechAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
